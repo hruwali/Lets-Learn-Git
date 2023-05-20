ac8ea024e82270c086bd90f0a9e96f8cb922e311
Himanshu <ruwalihimanshu15@gmail.com>
2023-05-20 14:43:14 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,13 @@
+public class Multiply {
+
+    public static void main(String[] args) {
+
+        int c= 23;
+        int d= 4;
+
+        System.out.println("The product is "+(c*d));
+
+        
+    }
+    
+}
